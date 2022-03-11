a9746505d3be130f1822549ef9566e77696bf732
david_s <78419094+DavidSurumen@users.noreply.github.com>
2022-03-11 21:17:30 +0300

    Task 6.Print numbers using putchar()

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * main -Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int x = 0;
+
+	while (x / 10 < 1)
+	{
+		putchar(x%10 + '0');
+		x++;
+	}
+	putchar('\n');
+	return (0);
+}
