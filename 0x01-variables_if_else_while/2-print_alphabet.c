6dc44f014c1c2ec1acf436936a30412b36467594
david_s <78419094+DavidSurumen@users.noreply.github.com>
2022-03-11 19:20:28 +0300

    Task 2.Print alphabets

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char k = 'a';
+
+	while(k <= 'z')
+	{
+		putchar(k);
+		k++;
+	}
+	putchar('\n');
+
+	return (0);
+}
