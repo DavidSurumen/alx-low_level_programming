b471cad67ccdd22ce4d588dd39a9c1fdeb6ca4cc
david_s <78419094+DavidSurumen@users.noreply.github.com>
2022-03-11 19:31:10 +0300

    Task 4. Print all letters of the alphabet except

---
+++ 0x01-variables_if_else_while/4-print_alphabet.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * main -Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char m = 'a';
+
+	while (m <= 'z')
+	{
+		if (m == 'q' || m == 'e')
+			continue;
+		else
+			putchar(m);
+		m++;
+	}
+	putchar('\n');
+	return (0);
+}
