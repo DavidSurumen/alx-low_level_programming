af97e2747f1eca038fdd7cb0d1726dc4725a4ad8
david_s <78419094+DavidSurumen@users.noreply.github.com>
2022-03-11 21:24:48 +0300

    Task 7.Print alphabets in revers

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * main -Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char k = 'z';
+
+	while (k >= 'a')
+	{
+		putchar(k);
+		k--;
+	}
+	putchar('\n');
+	return (0);
+}
