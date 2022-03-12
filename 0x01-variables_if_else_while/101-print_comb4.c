8e0079174f370ddedd3dbacf8aeea56a486fe8a4
david_s <78419094+DavidSurumen@users.noreply.github.com>
2022-03-12 13:44:12 +0300

    Advanced Task 11.Prints all possible different combinations of 3 digits

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -4,15 +4,15 @@
  * main -Entry point
  *
  * Description:
- * Program that prints all possible different combinations of three digits
- * Numbers must be separated by commas followed by a space
- * The three digits must be different
- * 012, 120, 102, 021, 201, 210 are considered the same combination of the three digits 0, 1, and 2
- * Print only the smallest combination of three digits
- * Numbers should be printed in ascending order, with three digits
- * You can only use the 'putchar' function to print to the console
- * You can only use 'putchar' six times maximum in your code
- * You are not allowed to use any variable of type 'char'
+ * Program that prints all possible different combinations of three digits.
+ * Numbers must be separated by commas followed by a space.
+ * The three digits must be different.
+ * 012, 120, 102, 021, 201, 210 are considered the same combination.
+ * Print only the smallest combination of three digits.
+ * Numbers should be printed in ascending order, with three digits.
+ * You can only use the 'putchar' function to print to the console.
+ * You can only use 'putchar' six times maximum in your code.
+ * You are not allowed to use any variable of type 'char'.
  *
  * Return: Always 0 (Success)
  */
@@ -33,7 +33,7 @@ int main(void)
 			putchar(b + '0');
 			putchar(a + '0');
 
-			if (num < 987)
+			if (num < 789)
 			{
 				putchar(44);
 				putchar(32);
