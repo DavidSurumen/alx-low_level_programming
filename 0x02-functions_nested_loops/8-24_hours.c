fb161ce77acf764ad4f0f5b900bd9ac38c432801
david_s <78419094+DavidSurumen@users.noreply.github.com>
2022-03-15 21:10:09 +0300

    solution for task 8

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -12,18 +12,18 @@ void jack_bauer(void)
 
 	while (hour < 24)
 	{
-		i = hours / 10; /* doubles digit in hours*/
-		j = hours % 10; /* singles digit */
+		i = hour / 10; /* doubles digit in hours*/
+		j = hour % 10; /* singles digit */
 
 		while (min < 60)
 		{
 			k = min / 10; /* doubles digit ni minutes */
-			k = min % 10; /* singles digit */
+			l = min % 10; /* singles digit */
 
 			_putchar('0' + i);
 			_putchar('0' + j);
 			_putchar(':');
-			_putchar('0' + k)'
+			_putchar('0' + k);
 			_putchar('0' + l);
 			_putchar('\n');
 
