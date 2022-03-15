559f0883fd2c5cb962146fdf31ff109512cf5a7f
david_s <78419094+DavidSurumen@users.noreply.github.com>
2022-03-15 19:06:26 +0300

    add solution for task0

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -3,7 +3,7 @@
 /**
  * main - Entry
  *
- * Return: Always 0 (Success)
+ * Return: 0 (Success)
  */
 void print_alphabet(void)
 {
@@ -16,6 +16,4 @@ void print_alphabet(void)
 		j++;
 	}
 	_putchar('\n');
-
-	return (0);
 }
