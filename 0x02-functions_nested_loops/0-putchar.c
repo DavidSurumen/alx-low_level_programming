90d0a5b302a286b19847111dd85033cb5c584638
david_s <78419094+DavidSurumen@users.noreply.github.com>
2022-03-15 15:09:42 +0300

    added readme and solution for task 0

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main -Entry point
+ *
+ * Description: print '_putchar' followed by a new line
+ * The program should return 0
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	printf("_putchar");
+	return (0);
+}
