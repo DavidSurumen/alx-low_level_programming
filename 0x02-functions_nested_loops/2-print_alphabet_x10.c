c1645a782ae3a6e421b5e2874d5a557af01aac10
david_s <78419094+DavidSurumen@users.noreply.github.com>
2022-03-15 19:23:55 +0300

    add solution for task2

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,7 +1,8 @@
 #include "main.h"
 
 /**
- * 2-print_alphabet_x10 - print alphabet 10 times in lowercase, followed by new line
+ * print_alphabet_x10 - print alphabet 10 times in lowercase
+ * 
  * use only '_putchar' function
  * _putchar can be used only twice
  * Return: 0 (Success)
