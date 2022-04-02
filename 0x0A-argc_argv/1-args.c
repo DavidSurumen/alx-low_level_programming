ef639ba8a1ec511c2713ce0afe419153f9c879f6
david_s <davidsurumen@gmail.com>
2022-04-02 01:33:07 +0000

    solution for task 1

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - prints the number of arguments
+ * @argc: holds the number of arguments passed
+ * @argv: pointer to string of arguments
+ * Return: Always 0 (Success)
+ */
+int main(int argc, char *argv[] __attribute__((unused)))
+{
+	if (argc > 0)
+		printf("%d\n", argc - 1);
+
+	return (0);
+}
