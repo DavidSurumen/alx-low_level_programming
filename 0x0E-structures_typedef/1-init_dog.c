7bc3cc103c5e59d36b14040975ab57f84c1984b8
david_s <davidsurumen@gmail.com>
2022-04-11 19:08:34 +0000

    solution to task 1

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,17 @@
+#include "dog.h"
+
+/**
+ * init_dog - initializes a variable of type struct dog
+ * @d: pointer to variable of type struct dog
+ * @name: pointer to member of struct
+ * @age: age of dog - member of struct
+ * @owner: pointer to owner - member of struct
+ *
+ * Return: nothing
+ */
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
+}
