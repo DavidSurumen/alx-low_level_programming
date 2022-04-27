e8f58f6b2e5b5326ffb0e4da50127d96f178fa96
david_s <davidsurumen@gmail.com>
2022-04-27 02:11:35 +0000

    correction

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -11,15 +11,15 @@ void free_listint2(listint_t **head)
 {
 	listint_t *tmp;
 
-	if (*head == NULL)
+	if (head == NULL)
 		return;
 
-	tmp = *head;
 	while (*head)
 	{
 		tmp = (*head)->next;
 		free(*head);
 		*head = tmp;
 	}
+
 	*head = NULL;
 }
