51b6d55f3da681c750e2a89eb14a9a51d31c31bd
david_s <davidsurumen@gmail.com>
2022-06-09 22:42:09 +0000

    task 6

--- 0x17-doubly_linked_lists/6-sum_dlistint.c
+++ 0x17-doubly_linked_lists/6-sum_dlistint.c
@@ -1,5 +1,11 @@
 #include "lists.h"
 
+/**
+ * sum_dlistint - gets the sum of all the data (n) of a doubly linked list
+ * @head: pointer to start of list
+ *
+ * Return: sum of all the data
+ */
 int sum_dlistint(dlistint_t *head)
 {
 	dlistint_t *tmp;
