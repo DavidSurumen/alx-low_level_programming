fffe799f7eab3d9387e8571fcab75569cc137f90
David Surumen <davidsurumen@gmail.com>
2023-01-08 19:57:06 +0300

    task 0 - search for a value using linear search

---
+++ 0x1E-search_algorithms/0-linear.c
@@ -0,0 +1,12 @@
+#include "search_algos.h"
+#include <stdio.h>
+
+/**
+ * linear_search: Sequencially search the whole list until a match
+ *	has been found or the whole list has been searched.
+ */
+int linear_search(int *array, size_t size, int value)
+{
+
+
+}
